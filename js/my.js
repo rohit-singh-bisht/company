2c4cad60ee3772a217e719e1204625ba65eb78cb
Rohit Singh Bisht <rohitsinghbisht@RohittekiMacBook-Air.local>
2021-10-11 01:22:41 +0530

    landing-page without banner

---
+++ js/my.js
@@ -0,0 +1,9 @@
+$('.testi-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: $('#prev'),
+    nextArrow: $('#next')
+  });
